6a6483f43425888e75b99daa79eebdc811111177
verissimo-sn <verissimo_sn@hotmail.com>
2020-09-08 11:09:08 -0300

    creating and styling home page

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+import logo from '../../assets/logo.svg';
+
+const CreatePoint = () => {
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt=""/>
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
+
+      <form>
+        <h1>Cadastro do ponto de coleta</h1>
+      </form>
+    </div>
+  );
+}
+
+export default CreatePoint;
